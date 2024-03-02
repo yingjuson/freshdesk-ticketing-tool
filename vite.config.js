1640daa3e635850a8e5dff150675bb0a38e90c89
jamesixw <james@j6winc.com>
2024-03-02 12:30:06 +0800

    fix: silence errors/warnings on build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            onwarn(warning, defaultHandler) {
+                if (warning.code === "SOURCEMAP_ERROR") {
+                    return;
+                }
+
+                defaultHandler(warning);
+            },
+        },
+    },
 });
