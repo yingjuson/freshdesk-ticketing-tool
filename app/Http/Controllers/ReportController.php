e8e883e8391fa8b90f9823bc2850a54bd30c62f7
jamesixw <james@j6winc.com>
2024-03-06 13:05:14 +0800

    feat: filter tickets between dates before chunking for export

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,32 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Ticket;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Storage;
 use Spatie\SimpleExcel\SimpleExcelWriter;
-
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ReportController extends Controller
 {
     public function export(Request $request) {
-        // $validated = $request->validate([
-        //     'file_type'     => 'required', 'in:csv,excel',
-        //     'start_date'    => 'required', 'date',
-        //     'end_date'      => 'required', 'date',
-        //     'fields.*'      => 'required'
-        // ]);
-
         $rows = [];
         $file_type = $request->input('file_type') == 'csv' ? '.csv' : '.xlsx';
 
-        Ticket::chunk(2000, function ($tickets) use (&$rows) {
-            foreach ($tickets->toArray() as $ticket) {
-                $rows[] = $ticket;
-            }
-        });
+        Ticket::whereBetween('created_at', [$request->input('start_date'), $request->input('end_date')])
+            ->chunk(2000, function ($tickets) use (&$rows) {
+                foreach ($tickets->toArray() as $ticket) {
+                    $rows[] = $ticket;
+                }
+            });
 
         $file_name = 'data_' . now()->format('YmdHis') . $file_type;
         $writer = SimpleExcelWriter::streamDownload($file_name)
